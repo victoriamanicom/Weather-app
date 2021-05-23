509ddb3d91ac65ac56e795b9fca564a5d932cefb
victoriamanicom <81752413+victoriamanicom@users.noreply.github.com>
2021-05-23 22:08:59 +0100

    Updated current location current weather

--- index.js
+++ index.js
@@ -67,13 +67,10 @@ let form = document.querySelector("#city-form");
 form.addEventListener("submit", searchCity);
 
 function showPosition(response) {
-  let temp = Math.round(response.data.main.temp);
-  celciusTemperature = Math.round(response.data.main.temp);
   let currentLoc = response.data.name;
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${currentLoc}`;
-  let displaytemp = document.querySelector("#temp");
-  displaytemp.innerHTML = `${temp}`;
+  displayWeather(response);
 }
 
 function showLocationTemperature(position) {
